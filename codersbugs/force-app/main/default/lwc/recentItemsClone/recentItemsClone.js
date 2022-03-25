882630685d2366592171b26e59a0272df0ad0b5f
codersbugs <admin@codersbugs.com>
2022-03-25 21:27:22 +0530

    Created clone of recent items
    Created clone of recent items

--- codersbugs/force-app/main/default/lwc/recentItemsClone/recentItemsClone.js
+++ codersbugs/force-app/main/default/lwc/recentItemsClone/recentItemsClone.js
@@ -1,13 +1,31 @@
-import { LightningElement, api} from 'lwc';
+import { LightningElement, track , api} from 'lwc';
+import getRecentRecords from "@salesforce/apex/RecentItemsController.getRecentRecords";
+const columns = [
+    { label: 'Id', fieldName: 'Id' },
+    { label: 'Name', fieldName: 'Name', type: 'Name' },
+    { label: 'Phone', fieldName: 'Phone', type: 'Phone' },
 
+];
 export default class RecentItemsClone extends LightningElement {
     @api ObjectName;
-    @api NumberOfRecordToDisplay;
-    @api LabelOfRecentRecords;
+    @api NumberOfRecordToDisplay = 5;
+    @api LabelOfRecentRecords = 'Recent records';
+    count = 0;
+    data = [];
+    columns = columns;
 
     renderedCallback() { 
-        console.log(' ObjectName ', this.ObjectName);
-        console.log(' LabelOfRecentRecords ', this.LabelOfRecentRecords);
-        console.log(' NumberOfRecordToDisplay ', this.NumberOfRecordToDisplay);
+        if(this.count<3) { 
+
+            getRecentRecords({ doDoName: this.ObjectName , numberOfRecords: this.NumberOfRecordToDisplay})
+            .then((data) => {
+                this.data = data;
+                console.log('getting records');
+            })
+            .catch((error) => { 
+                console.log('Error in record fetch')
+            });
+            this.count++;
+        }  
     }
 }
