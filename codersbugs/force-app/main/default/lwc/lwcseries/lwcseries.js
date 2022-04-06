e9fc0824240fd83c991609324bcefb1a691170f0
codersbugs <admin@codersbugs.com>
2022-04-06 22:46:00 +0530

    created lwc series
    created lwc series

--- codersbugs/force-app/main/default/lwc/lwcseries/lwcseries.js
+++ codersbugs/force-app/main/default/lwc/lwcseries/lwcseries.js
@@ -1,17 +1,22 @@
-import { LightningElement , track , wire } from 'lwc';
+import { LightningElement , track , wire ,api} from 'lwc';
 import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import Image from '@salesforce/resourceUrl/Image';
 
 export default class Lwcseries extends LightningElement {
     @track showButton = false;
+    @api Name = '';
     lstFields = [];
     newObjectName = '';
     objectName = '';
+    @track ImageToShow = Image;
     getName(event) {
         this.showButton = true
         this.objectName = event.target.value;
     }
     connectedCallback() { 
-        console.log('connectedCallback is call');
+        console.log('Image ', Image);
+        this.showTosat('Success','connectedCallback is call','success');
     }
     changeObjectName() {
         this.newObjectName = this.objectName;
@@ -23,10 +28,13 @@ export default class Lwcseries extends LightningElement {
             for (let key in data.fields) {
                 this.lstFields.push({ value: key, label: data.fields[key].label });
             }
-            console.log(' lstFields ', JSON.stringify(this.lstFields));
+            this.showTosat('Info','fields loaded','info');
         }
         else if (error) { 
-            console.log('error  lstFields ')
+            this.showTosat('Error','While while getting fields ','error');
         }
+    }
+    showTosat (title, message, variant) {
+        this.dispatchEvent(new ShowToastEvent ({ title: title, message: message, variant: variant }));
     }
 }
